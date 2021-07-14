b6b1a26361633581a251f0b6822918e257d0ac5e
Shashank Shekhar <52148699+Shekhar-Shashank@users.noreply.github.com>
2021-07-14 09:50:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-var userArray = [2,5,1,2,3,5,1,2,4];  //should return 2
-var arrayexample2 = [2,1,1,2,3,5,1,2,4]; //should return 1
-var arrayexample3 = [2,3,4,5];  //should return undefinded
+let userArray = [2,5,1,2,3,5,1,2,4];  //should return 2
+let arrayexample2 = [2,1,1,2,3,5,1,2,4]; //should return 1
+let arrayexample3 = [2,3,4,5];  //should return undefinded
 
 let newObj = {};
 
 for(let i=0; i<userArray.length;i++){
-  newObj[arrayexample2[i]]=0
+  newObj[arrayexample2[i]]=0;
 }
 //console.log([...newMap.entries()]);
 
-for(let j of arrayexample3){
-  //console.log(j);
+for(let j of arrayexample2){  // change the array here
+
   if(j){
-    newObj[j] = newObj[j]+1
+    newObj[j] += 1;
   }
-  //console.log(newObj)
-   if(newObj[j]>1){
+  if(newObj[j]>1){
     console.log(j);
     break;
-  }
- 
-  }
-  console.log(undefined)
- 
+  }}
+  console.log(undefined);
  
   
